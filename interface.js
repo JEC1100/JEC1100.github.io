0be359e356e8fe8b211ae2d762754732f9f3d687
lushcodes <lushcodes@gmail.com>
2021-08-10 16:33:54 +0100

    Updated interface

--- interface.js
+++ interface.js
@@ -2,32 +2,30 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-
-
-
 // new note variable
-let note = new Note();
-let notesArray = [];
+// let notesArray = [];
 
 // Creates a new div and enters it into the body
-createNewDiv() {
+createNewDiv(note) {
   let div = document.createElement('div');
   div.className = 'note-div';
-  document.getElementsByTagName('body')[0].appendChild(div);
+  document.getElementById('right-side')[0].appendChild(div);
   div.innerHTML = note.title;
 };
 
 // gets text input and adds to note
 populateNoteText() {
- note.text = document.querySelector('#text-input').value;
+  let input = document.querySelector('#text-input').value;
+  let note = new Note(input);
+  createNewDiv(note);
+  // notesArray.push(note);
 };
 
 // When button is clicked
 document.querySelector('#create').addEventListener('click', () => {
   populateNoteText();
-  createNewDiv();
-})
-
+});
+});
 
 
 
