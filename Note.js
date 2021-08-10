a544461e0a310ff4446532cd9fa5e5aba15eea88
Brett Karlson <brettjkarlson@gmail.com>
2021-08-10 15:55:18 +0100

    initial tests passing

--- Note.js
+++ Note.js
@@ -1,1 +1,12 @@
 "use strict";
+
+class Note {
+  constructor(text) {
+    this.text = text;
+    this.title = this.text.substring(0,20);
+  }
+
+// title() {
+//   return 
+//   }
+ } 
--- NoteSpec.js
+++ NoteSpec.js
@@ -1,1 +1,28 @@
 "use strict";
+
+function testText() {
+  let actual = "This is a test note more than 20 characters";
+  let note = new Note(actual);
+  
+  let expected = note.text
+
+  if (actual == expected) {
+    console.log("pass");
+  } else 
+  console.log("testText failed");
+}
+testText();
+
+function testTitle() {
+  let note = new Note("This is a test note more than 20 characters");
+  console.log(note);
+  let noteTitle = note.title;
+
+  if (noteTitle.length > 20) {
+    throw new Error("Title is more than 20 characters");
+  } else 
+  console.log("pass");
+
+};
+
+testTitle();
