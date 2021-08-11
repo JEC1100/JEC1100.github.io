aaf68888c7d8af67552bf8042e4c10caf6b63be0
Matt Firmston <mattfirmston@gmail.com>
2021-08-11 18:18:35 +0100

    Text gets emojified before being added to note

--- interface.js
+++ interface.js
@@ -1,19 +1,26 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-  
   function createNewDiv(note) {
     const div = document.createElement('div');
     div.className = 'note-div';
     div.innerHTML = note.title + "...";
-    // Took out [0]
     document.getElementById('right-side').appendChild(div);
   };
   
   function createNoteFromText() {
-    const note = new Note(document.querySelector('#text-input').value);
-    createNewDiv(note);
-    document.querySelector('#text-input').value = "";
+    const text = document.querySelector('#text-input').value
+    fetch("https://makers-emojify.herokuapp.com/", {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({text: text})
+    }).then((response) => {
+      return response.json()
+    }).then((json) => {
+      const note = new Note(json.emojified_text);
+      createNewDiv(note);
+      document.querySelector('#text-input').value = "";
+    })
   }
   
   document.querySelector('#create').addEventListener('click', () => {
